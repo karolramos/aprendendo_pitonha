4d209f95a88dbf18142bc0dfca540d4bff16056f
karolramos <karollamendonca@gmail.com>
2021-09-26 15:20:09 -0300

    adc palavras e arte externa

--- picking_random_words_checking_answers.py
+++ picking_random_words_checking_answers.py
@@ -1,67 +1,10 @@
 import random
+from art import stages_hangman, logo_hangman
+from words_hangman import word_list
 
-stages = ['''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-''']
-
+print(logo_hangman)
 
 # Escolher uma palavra aleatória da lista de palavras
-word_list = ['banana', 'maçã', 'uva', 'mamão', 'acerola', 'graviola', 'manga', 'laranja', 'abacate', 'melancia',
-             'melao']
 chosen_word = random.choice(word_list)
 word_length = len(chosen_word)
 end_of_game = False
@@ -73,11 +16,13 @@ lives = 6
 display = []
 for _ in range(word_length):
     display += "_"
-print(display)
 
 while not end_of_game:
     # Escolher uma letra, transformar em minúscula e imprimir a letra escolhida
     guess = input('Escolha uma letra: ').lower()
+
+    if guess in display:
+        print(f"You've already guessed {guess}")
 
     # Fazendo um loop e verificando se cada uma letra da palavra escolhida é compatível e adc no lugar certo
     for position in range(word_length):
@@ -86,6 +31,7 @@ while not end_of_game:
             display[position] = letter
 
     if guess not in chosen_word:
+        print(f"You guessed {guess}, that's not in the word. You lose a life")
         lives -= 1
         if lives == 0:
             end_of_game = True
@@ -97,4 +43,4 @@ while not end_of_game:
         end_of_game = True
         print('You win! : )')
 
-    print(stages[lives])
+    print(stages_hangman[lives])
